646f52eae54f8873fac5cb7597cd6b96dbdad0fe
Kolamin <gofranich@gmail.com>
2019-10-17 21:53:04 +0600

    Session2

--- src/FirstServlet.java
+++ src/FirstServlet.java
@@ -1,4 +1,6 @@
-import javax.servlet.RequestDispatcher;
+import somePackage.Cart;
+
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -8,17 +10,32 @@ public class FirstServlet extends javax.servlet.http.HttpServlet {
     }
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        HttpSession session = request.getSession();
 
-        String name = request.getParameter("name");
-        String surname = request.getParameter("surname");
+        String user = (String)session.getAttribute("current_user");
+        if(user == null){
 
-        PrintWriter pw = response.getWriter();
+        }else{
 
-        pw.println("<html>");
-        pw.println("<h1>Hello, " + name +" " + surname + " </h1>");
-        pw.println("</html>");
+        }
 
-        RequestDispatcher dispatcher = request.getRequestDispatcher("/testJsp.jsp");
-        dispatcher.forward(request, response);
+//        Cart cart = (Cart) session.getAttribute("cart");
+//
+//        String name = request.getParameter("name");
+//        int quantity = Integer.parseInt(request.getParameter("quantity"));
+//        if (cart == null) {
+//            cart = new Cart();
+//
+//            cart.setName(name);
+//            cart.setQuantity(quantity);
+//        }
+//
+//        session.setAttribute("cart", cart);
+//
+//        getServletContext().
+//                getRequestDispatcher("/showCart.jsp").
+//                forward(request, response);
+
+
     }
 }
